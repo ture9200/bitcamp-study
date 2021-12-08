2b7ea66a7768a11b76842d05a55f7d030fd4659c
ture9200 <didtjddms09@naver.com>
2021-12-08 21:22:47 +0900

    ,

---
+++ java-lang/app/src/main/java/com/eomcs/lang/ex05/Exam0530.java
@@ -0,0 +1,13 @@
+package com.eomcs.lang.ex05;
+
+public class Exam0530 {
+  public static void main(String[] args) {
+    int age = 20;
+
+    // 표현식은 반드시 값을 리턴해야 한다.
+    // String str = (age > 18) ? System.out.println("성인이다.") : System.out.println("미성년자이다.");
+
+  }
+}
+
+
