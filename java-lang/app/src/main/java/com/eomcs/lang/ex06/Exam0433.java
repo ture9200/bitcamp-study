a0567c528c9a023b8d05ed857378b4f28f45198e
ture9200 <didtjddms09@naver.com>
2021-12-16 16:15:58 +0900

    commit

--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0433.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0433.java
@@ -1,6 +1,6 @@
 package com.eomcs.lang.ex06;
 
-// # 흐름 제어문 - for 중첩과 break
+// # 흐름 제어문 - for 중첩과 continue
 //
 public class Exam0433 {
   public static void main(String[] args) {
@@ -9,6 +9,8 @@ public class Exam0433 {
       for (int j = 1; j <= i; j++) {
         if (j % 2 == 0) // 짝수
           continue; // 다음 줄로 가지 않고 '변수증가문'(2번째:j)으로 이동한다.
+        // 짝수를 건너뛰고 출력 
+        // while문은 조건문으로 이동 
         System.out.print(j + " ");
       }
       System.out.println();
