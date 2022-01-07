cc946a1acf311422d84dae237d999f2f52ec8dca
ture9200 <didtjddms09@naver.com>
2022-01-07 11:02:13 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
@@ -21,12 +21,7 @@ public class BookController {
 
   @RequestMapping("/book/add")
   public Object add(Book book) {
-    book.setReadDate(new Date(System.currentTimeMillis()));
-    //Calendar calendar =Calendar.getInstance();
-    //현재 시간을 일반 시간이 아닌 요일, 오전/오후 등인 달력 기준으로 표기해주는 방식이다.  
-    //board.setCreatedDate(new Date(System.currentTimeMillis()));
-    //함수를 사용해서 현재 시간을 받아오고 board 에 날짜를 설정하라는 뜻. 
-
+    book.setReadDate(new Date(System.currentTimeMillis()));   
     bookList.add(book);
     return bookList;
   }
@@ -39,11 +34,7 @@ public class BookController {
       return "";
     }
     Book book = (Book)bookList.get(index);
-    // board 리스트 항목을 Book로 형변환해서 book에 저장한다. 
-    //book.setViewCount(book.getViewCount()+1);
-    // board의 조회수를 하나씩 늘린다. 
-    return book;
-    // 그렇게 해서 ArrayList의 항목을 리턴한다. 
+    return book; 
   }
 
   @RequestMapping("/book/update")
@@ -52,12 +43,7 @@ public class BookController {
       return 0;
     }
     Book old = (Book)bookList.get(index);
-    // boardlist 항목들을 Board로 형변환해서 old에 저장한다. 
-    //book.setViewCount( old.getViewCount());
-    // 기존조회수를 viewCount에 담고 
     book.setReadDate(old.getReadDate()); 
-    // 기존 날짜도 게시글 날짜에 저장이 된다. 
-
     return bookList.set(index, book) == null ? 0 :1; //한번 더 거르겠다는 말!! 
   }
 
