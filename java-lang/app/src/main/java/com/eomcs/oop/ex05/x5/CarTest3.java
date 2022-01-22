ee40f31a1f827ba92865ba236850f4e9152972d5
ture9200 <didtjddms09@naver.com>
2022-01-22 15:15:34 +0900

    change in oop

--- java-lang/app/src/main/java/com/eomcs/oop/ex05/x5/CarTest3.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex05/x5/CarTest3.java
@@ -9,8 +9,10 @@ public class CarTest3 {
     electricOption.chargeBattery(100);
 
     Trailer trailer = new Trailer(sedan);
+    // 트레일러를 sedan으로 받았다. 
     trailer.start();
     trailer.run();
+    // run을 호출하면 car한테 위임을 하고 덧붙이고 싶은 기능이 있으면 수행한다. 
     trailer.stop();
 
     System.out.println("----------------------------");
