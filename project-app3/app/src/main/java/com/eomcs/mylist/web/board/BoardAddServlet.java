586a4655b6d8f608e1fd793c22f7d6ca7f902350
ture9200 <didtjddms09@naver.com>
2022-04-12 16:35:52 +0900

    2022-04-12 톰캣서버로 member와 board 부분 동작하게 하기

--- project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardAddServlet.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardAddServlet.java
@@ -1,4 +1,4 @@
-package com.eomcs.app3;
+package com.eomcs.mylist.web.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,16 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet({"/hello3", "/hello4", "/hello5"}) // 서블릿 컨테이너에게 이 클래스가 /hello 요청을 처리하는 서블릿임을 알려준다.
-public class Hello3Servlet extends HttpServlet {
+@WebServlet("/board/add") 
+public class BoardAddServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    System.out.println("service() 호출!");
-
-    // HTTP 클라이언트가 name이란 이름으로 보내 온 파라미터 값을 읽는다.
-    String name = req.getParameter("name");
 
     // HTTP 클라이언트에게 보낼 콘텐트의 MIME 타입을 설정한다.
     resp.setContentType("text/plain;charset=UTF-8");
@@ -27,6 +23,6 @@ public class Hello3Servlet extends HttpServlet {
     PrintWriter out = resp.getWriter();
 
     // HTTP 클라이언트에게 콘텐트를 출력한다.
-    out.printf("%s 님 환영합니다!", name);
+    out.println("게시글 등록");
   }
 }
