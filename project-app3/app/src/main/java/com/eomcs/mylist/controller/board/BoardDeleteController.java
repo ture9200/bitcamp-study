7371f5d6ff6c30cbcca9cc23960e94ef229544ae
ture9200 <didtjddms09@naver.com>
2022-04-18 11:34:43 +0900

    mvc servlet continue

--- project-app3/app/src/main/java/com/eomcs/mylist/controller/board/BoardDeleteController.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/controller/board/BoardDeleteController.java
@@ -1,4 +1,4 @@
-package com.eomcs.mylist.web.board;
+package com.eomcs.mylist.controller.board;
 
 import java.io.IOException;
 import javax.servlet.ServletContext;
@@ -13,39 +13,34 @@ import com.eomcs.mylist.service.BoardService;
 
 @SuppressWarnings("serial")
 @WebServlet("/board/delete") 
-public class BoardDeleteServlet extends HttpServlet {
+public class BoardDeleteController extends HttpServlet {
 
-  
   BoardService boardService;
 
   @Override
   public void init() throws ServletException {
-    // BoardService 객체를 웹애플리케이션 보관소에서 꺼낸다.
     ServletContext 웹애플리케이션보관소 = this.getServletContext();
     boardService = (BoardService) 웹애플리케이션보관소.getAttribute("boardService");
   }
-  
+
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     try {
-    Board board = new Board();
-    int no= Integer.parseInt(req.getParameter("no"));
-    
-    Member loginUser= (Member) req.getSession().getAttribute("loginUser");
-    board.setWriter(loginUser);
-    
-    boardService.delete(board);
+      Board board = new Board();
+      board.setNo(Integer.parseInt(request.getParameter("no")));
 
-    
-    resp.sendRedirect("list");
-    
-    }catch (Exception e) {
-      req.setAttribute("exception", e);
-      //포워드하기전에 출력한 콘텐트가 있다면 모두 버리고 다른 서블릿에게 책임을 위임한다. 
-      req.getRequestDispatcher("/error").forward(req, resp);
+      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+      board.setWriter(loginUser);
+
+      boardService.delete(board);
+
+      response.sendRedirect("list");
+
+    } catch (Exception e) {
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
     }
-    
   }
 }
--- project-app3/app/src/main/webapp/jsp/board/form.jsp
+++ project-app3/app/src/main/webapp/jsp/board/form.jsp
@@ -19,7 +19,7 @@
 
 <div id="content">
 <h1>게시글 등록</h1>
-<form action="add.jsp" method='post'>
+<form action="add" method='post'>
 제목*: <input name="title" type="text"><br>
 내용*: <textarea name="content" cols="50" rows="10"></textarea><br>
 별표(*) 항목은 필수 입력입니다.<br>
@@ -36,7 +36,7 @@
 
 <script>
 document.querySelector('#cancel-btn').onclick = () => {
-  location.href = 'list.jsp';
+  location.href = 'list';
 }
 </script>
 
